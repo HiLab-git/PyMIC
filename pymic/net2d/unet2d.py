3f826c0000fb28fca76b35380e4b4e25f52bb7cb
Guotai Wang <wguotai@gmail.com>
2020-04-15 17:37:30 +0800

    add comments

--- pymic/net2d/unet2d.py
+++ pymic/net2d/unet2d.py
@@ -1,20 +1,33 @@
 # -*- coding: utf-8 -*-
+"""
+An implementation of the U-Net paper:
+    Olaf Ronneberger, Philipp Fischer, Thomas Brox:
+    U-Net: Convolutional Networks for Biomedical Image Segmentation. 
+    MICCAI (3) 2015: 234-241
+Note that there are some modifications from the original paper, such as
+the use of batch normalization, dropout, and leaky relu here.
+"""
 from __future__ import print_function, division
 
 import torch
 import torch.nn as nn
+import numpy as np 
 
 class ConvBlock(nn.Module):
     """two convolution layers with batch norm and leaky relu"""
-    def __init__(self,in_channels, out_channels):
+    def __init__(self,in_channels, out_channels, dropout_p):
+        """
+        dropout_p: probability to be zeroed
+        """
         super(ConvBlock, self).__init__()
         self.conv_conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_channels),
             nn.LeakyReLU(),
+            nn.Dropout(dropout_p),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_channels),
-            nn.LeakyReLU(),
+            nn.LeakyReLU()
         )
        
     def forward(self, x):
@@ -22,11 +35,11 @@ class ConvBlock(nn.Module):
 
 class DownBlock(nn.Module):
     """Downsampling followed by ConvBlock"""
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, dropout_p):
         super(DownBlock, self).__init__()
         self.maxpool_conv = nn.Sequential(
             nn.MaxPool2d(2),
-            ConvBlock(in_channels, out_channels)
+            ConvBlock(in_channels, out_channels, dropout_p)
         )
 
     def forward(self, x):
@@ -34,27 +47,22 @@ class DownBlock(nn.Module):
 
 class UpBlock(nn.Module):
     """Upssampling followed by ConvBlock"""
-    def __init__(self, in_channels1, in_channels2, out_channels, 
-                 bilinear=True, dropout = False):
+    def __init__(self, in_channels1, in_channels2, out_channels, dropout_p,
+                 bilinear=True):
         super(UpBlock, self).__init__()
         self.bilinear = bilinear
-        self.dropout  = dropout
         if bilinear:
             self.conv1x1 = nn.Conv2d(in_channels1, in_channels2, kernel_size = 1)
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         else:
             self.up = nn.ConvTranspose2d(in_channels1, in_channels2, kernel_size=2, stride=2)
-        if dropout:
-            self.drop = nn.Dropout(0.5)
-        self.conv = ConvBlock(in_channels2 * 2, out_channels)
+        self.conv = ConvBlock(in_channels2 * 2, out_channels, dropout_p)
 
     def forward(self, x1, x2):
         if self.bilinear:
             x1 = self.conv1x1(x1)
         x1 = self.up(x1)
         x = torch.cat([x2, x1], dim=1)
-        if self.dropout:
-            x = self.drop(x)
         return self.conv(x)
 
 class UNet2D(nn.Module):
@@ -68,15 +76,15 @@ class UNet2D(nn.Module):
         self.dropout   = self.params['dropout']
         assert(len(self.ft_chns) == 5)
 
-        self.in_conv= ConvBlock(self.in_chns, self.ft_chns[0])
-        self.down1  = DownBlock(self.ft_chns[0], self.ft_chns[1])
-        self.down2  = DownBlock(self.ft_chns[1], self.ft_chns[2])
-        self.down3  = DownBlock(self.ft_chns[2], self.ft_chns[3])
-        self.down4  = DownBlock(self.ft_chns[3], self.ft_chns[4])
-        self.up1    = UpBlock(self.ft_chns[4], self.ft_chns[3], self.ft_chns[3], dropout = self.dropout)
-        self.up2    = UpBlock(self.ft_chns[3], self.ft_chns[2], self.ft_chns[2], dropout = self.dropout)
-        self.up3    = UpBlock(self.ft_chns[2], self.ft_chns[1], self.ft_chns[1])
-        self.up4    = UpBlock(self.ft_chns[1], self.ft_chns[0], self.ft_chns[0])
+        self.in_conv= ConvBlock(self.in_chns, self.ft_chns[0], self.dropout[0])
+        self.down1  = DownBlock(self.ft_chns[0], self.ft_chns[1], self.dropout[1])
+        self.down2  = DownBlock(self.ft_chns[1], self.ft_chns[2], self.dropout[2])
+        self.down3  = DownBlock(self.ft_chns[2], self.ft_chns[3], self.dropout[3])
+        self.down4  = DownBlock(self.ft_chns[3], self.ft_chns[4], self.dropout[4])
+        self.up1 = UpBlock(self.ft_chns[4], self.ft_chns[3], self.ft_chns[3], dropout_p = 0.0) 
+        self.up2 = UpBlock(self.ft_chns[3], self.ft_chns[2], self.ft_chns[2], dropout_p = 0.0) 
+        self.up3 = UpBlock(self.ft_chns[2], self.ft_chns[1], self.ft_chns[1], dropout_p = 0.0) 
+        self.up4 = UpBlock(self.ft_chns[1], self.ft_chns[0], self.ft_chns[0], dropout_p = 0.0) 
     
         self.out_conv = nn.Conv2d(self.ft_chns[0], self.n_class,  
             kernel_size = 3, padding = 1)
@@ -105,3 +113,22 @@ class UNet2D(nn.Module):
             output = torch.reshape(output, new_shape)
             output = torch.transpose(output, 1, 2)
         return output
+
+if __name__ == "__main__":
+    params = {'in_chns':4,
+              'feature_chns':[2, 8, 32, 48, 64],
+              'dropout':  [0, 0, 0.3, 0.4, 0.5],
+              'class_num': 2,
+              'bilinear': True,
+              'acti_func': 'relu'}
+    Net = UNet2D(params)
+    Net = Net.double()
+
+    x  = np.random.rand(4, 4, 10, 96, 96)
+    xt = torch.from_numpy(x)
+    xt = torch.tensor(xt)
+    
+    y = Net(xt)
+    print(len(y.size()))
+    y = y.detach().numpy()
+    print(y.shape)
