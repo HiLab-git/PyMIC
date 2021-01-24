18ef6d1a22a1ebf9b36afe03f8871ca9c14291e6
Guotai Wang <wguotai@gmail.com>
2021-01-24 09:55:06 +0800

    enable deterministic

--- pymic/net_run/net_run_agent.py
+++ pymic/net_run/net_run_agent.py
@@ -38,7 +38,7 @@ def seed_torch(seed=1):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
+    torch.cuda.manual_seed_all(seed) # if using multi-GPU.
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
 
