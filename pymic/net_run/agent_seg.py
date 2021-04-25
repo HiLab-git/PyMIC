3c18d3eec2fcbc95a43add621928098294ed727b
Guotai Wang <wguotai@gmail.com>
2021-04-25 11:37:42 +0800

    use an object for inference, support sliding window and test time augmentatin

--- pymic/net_run/agent_seg.py
+++ pymic/net_run/agent_seg.py
@@ -24,7 +24,7 @@ from pymic.io.nifty_dataset import NiftyDataset
 from pymic.transform.trans_dict import TransformDict
 from pymic.net.net_dict_seg import SegNetDict
 from pymic.net_run.agent_abstract import NetRunAgent
-from pymic.net_run.infer_func import volume_infer
+from pymic.net_run.infer_func import Inferer
 from pymic.loss.loss_dict_seg import SegLossDict
 from pymic.loss.seg.util import get_soft_label
 from pymic.loss.seg.util import reshape_prediction_and_ground_truth
@@ -183,16 +183,15 @@ class SegmentationAgent(NetRunAgent):
             self.optimizer.step()
             self.scheduler.step()
 
-            train_loss += loss.item()
+            train_loss = train_loss + loss.item()
             # get dice evaluation for each class
-            with torch.no_grad():
-                if(isinstance(outputs, tuple) or isinstance(outputs, list)):
-                    outputs = outputs[0] 
-                outputs_argmax = torch.argmax(outputs, dim = 1, keepdim = True)
-                soft_out = get_soft_label(outputs_argmax, class_num, self.tensor_type)
-                soft_out, labels_prob = reshape_prediction_and_ground_truth(soft_out, labels_prob) 
-                dice_list = get_classwise_dice(soft_out, labels_prob)
-                train_dice_list.append(dice_list.detach().cpu().numpy())
+            if(isinstance(outputs, tuple) or isinstance(outputs, list)):
+                outputs = outputs[0] 
+            outputs_argmax = torch.argmax(outputs, dim = 1, keepdim = True)
+            soft_out       = get_soft_label(outputs_argmax, class_num, self.tensor_type)
+            soft_out, labels_prob = reshape_prediction_and_ground_truth(soft_out, labels_prob) 
+            dice_list = get_classwise_dice(soft_out, labels_prob)
+            train_dice_list.append(dice_list.cpu().numpy())
         train_avg_loss = train_loss / iter_valid
         train_cls_dice = np.asarray(train_dice_list).mean(axis = 0)
         train_avg_dice = train_cls_dice.mean()
@@ -202,24 +201,22 @@ class SegmentationAgent(NetRunAgent):
         return train_scalers
         
     def validation(self):
-        class_num   = self.config['network']['class_num']
-        infer_sliding_window = self.config['testing']['infer_sliding_window']
-        window_size    = self.config['testing']['sliding_window_size']
-        window_stride  = self.config['testing']['sliding_window_stride']
-        output_num     = self.config['testing'].get('output_num', 1)
+        class_num = self.config['network']['class_num']
+        infer_cfg = self.config['testing']
+        infer_cfg['class_num'] = class_num
+        
         valid_loss_list = []
         valid_dice_list = []
         validIter  = iter(self.valid_loader)
         with torch.no_grad():
             self.net.eval()
+            infer_obj = Inferer(self.net, infer_cfg)
             for data in validIter:
                 inputs      = self.convert_tensor_type(data['image'])
                 labels_prob = self.convert_tensor_type(data['label_prob'])
                 inputs, labels_prob  = inputs.to(self.device), labels_prob.to(self.device)
-                batch_n     = inputs.shape[0]
-
-                outputs = volume_infer(inputs, self.net, class_num, 
-                    infer_sliding_window, window_size, window_stride, output_num)
+                batch_n = inputs.shape[0]
+                outputs = infer_obj.run(inputs)
 
                 # The tensors are on CPU when calculating loss for validation data
                 loss = self.get_loss_value(data, inputs, outputs, labels_prob)
@@ -352,42 +349,15 @@ class SegmentationAgent(NetRunAgent):
                         print('dropout layer')
                         m.train()
                 self.net.apply(test_time_dropout)
-        output_dir   = self.config['testing']['output_dir']
-        class_num    = self.config['network']['class_num']
-        infer_sliding_window = self.config['testing']['infer_sliding_window']
-        window_size    = self.config['testing']['sliding_window_size']
-        window_stride  = self.config['testing']['sliding_window_stride']
-        label_source = self.config['testing'].get('label_source', None)
-        label_target = self.config['testing'].get('label_target', None)
-        filename_ignore_dir     = self.config['testing'].get('filename_ignore_dir', True)
-        filename_replace_source = self.config['testing'].get('filename_replace_source', None)
-        filename_replace_target = self.config['testing'].get('filename_replace_target', None)
-        save_probability = self.config['testing'].get('save_probability', False)
-        if(not os.path.exists(output_dir)):
-            os.mkdir(output_dir)
 
-        # automatically infer outupt shape
-        # if(mini_patch_inshape is not None):
-        #     patch_inshape = [1, self.config['dataset']['modal_num']] + mini_patch_inshape
-        #     testx = np.random.random(patch_inshape)
-        #     testx = torch.from_numpy(testx)
-        #     testx = torch.tensor(testx)
-        #     testx = self.convert_tensor_type(testx)
-        #     testx = testx.to(device)
-        #     testy = self.net(testx)
-        #     if(isinstance(testy, tuple) or isinstance(testy, list)):
-        #         testy = testy[0] 
-        #     testy = testy.detach().cpu().numpy()
-        #     mini_patch_outshape = testy.shape[2:]
-        #     print('mini patch in shape', mini_patch_inshape)
-        #     print('mini patch out shape', mini_patch_outshape)
-        
+        infer_cfg = self.config['testing']
+        infer_cfg['class_num'] = self.config['network']['class_num']
+        infer_obj = Inferer(self.net, infer_cfg)
         infer_time_list = []
         with torch.no_grad():
             for data in self.test_loder:
                 images = self.convert_tensor_type(data['image'])
                 images = images.to(device)
-                names  = data['names']
     
                 # for debug
                 # for i in range(images.shape[0]):
@@ -400,51 +370,56 @@ class SegmentationAgent(NetRunAgent):
                 # continue
                 start_time = time.time()
                 
-                pred = volume_infer(images, self.net, class_num, 
-                    infer_sliding_window, window_size, window_stride)
+                pred = infer_obj.run(images)
                 # convert tensor to numpy
                 if isinstance(pred, (tuple, list)):
                     pred = pred[0]
                 data['predict'] = pred.cpu().numpy() 
-
                 # inverse transform
                 for i in reversed(range(len(self.transform_list))):
                     if (self.transform_list[i].inverse):
                         data = self.transform_list[i].inverse_transform_for_prediction(data) 
                 infer_time = time.time() - start_time
                 infer_time_list.append(infer_time)
-
-                prob = scipy.special.softmax(data['predict'], axis = 1) 
-                output = np.asarray(np.argmax(prob,  axis = 1), np.uint8)
-                if((label_source is not None) and (label_target is not None)):
-                    output = convert_label(output, label_source, label_target)
-                # save the output and (optionally) probability predictions
-                root_dir  = self.config['dataset']['root_dir']
-                for i in range(len(names)):
-                    save_name = names[i].split('/')[-1] if filename_ignore_dir else \
-                        names[i].replace('/', '_')
-                    print(save_name)
-                    if((filename_replace_source is  not None) and (filename_replace_target is not None)):
-                        save_name = save_name.replace(filename_replace_source, filename_replace_target)
-                    save_name = "{0:}/{1:}".format(output_dir, save_name)
-                    save_nd_array_as_image(output[i], save_name, root_dir + '/' + names[i])
-                    save_name_split = save_name.split('.')
-                    if('.nii.gz' in save_name):
-                        save_prefix = '.'.join(save_name_split[:-2])
-                        save_format = 'nii.gz'
-                    else:
-                        save_prefix = '.'.join(save_name_split[:-1])
-                        save_format = save_name_split[-1]
-                    if(save_probability):
-                        class_num = prob.shape[1]
-                        for c in range(0, class_num):
-                            temp_prob = prob[i][c]
-                            prob_save_name = "{0:}_prob_{1:}.{2:}".format(save_prefix, c, save_format)
-                            if(len(temp_prob.shape) == 2):
-                                temp_prob = np.asarray(temp_prob * 255, np.uint8)
-                            save_nd_array_as_image(temp_prob, prob_save_name, root_dir + '/' + names[i])
-
+                self.save_ouputs(data)
         infer_time_list = np.asarray(infer_time_list)
-        time_avg = infer_time_list.mean()
-        time_std = infer_time_list.std()
+        time_avg, time_std = infer_time_list.mean(), infer_time_list.std()
         print("testing time {0:} +/- {1:}".format(time_avg, time_std))
+
+    def save_ouputs(self, data):
+        output_dir = self.config['testing']['output_dir']
+        ignore_dir = self.config['testing'].get('filename_ignore_dir', True)
+        save_prob  = self.config['testing'].get('save_probability', False)
+        if(not os.path.exists(output_dir)):
+            os.mkdir(output_dir)
+
+        names, pred = data['names'], data['predict']
+        prob   = scipy.special.softmax(pred, axis = 1) 
+        output = np.asarray(np.argmax(prob,  axis = 1), np.uint8)
+
+        # save the output and (optionally) probability predictions
+        root_dir  = self.config['dataset']['root_dir']
+        for i in range(len(names)):
+            save_name = names[i].split('/')[-1] if ignore_dir else \
+                names[i].replace('/', '_')
+            print(save_name)
+            save_name = "{0:}/{1:}".format(output_dir, save_name)
+            save_nd_array_as_image(output[i], save_name, root_dir + '/' + names[i])
+            save_name_split = save_name.split('.')
+
+            if(not save_prob):
+                continue
+            if('.nii.gz' in save_name):
+                save_prefix = '.'.join(save_name_split[:-2])
+                save_format = 'nii.gz'
+            else:
+                save_prefix = '.'.join(save_name_split[:-1])
+                save_format = save_name_split[-1]
+            
+            class_num = prob.shape[1]
+            for c in range(0, class_num):
+                temp_prob = prob[i][c]
+                prob_save_name = "{0:}_prob_{1:}.{2:}".format(save_prefix, c, save_format)
+                if(len(temp_prob.shape) == 2):
+                    temp_prob = np.asarray(temp_prob * 255, np.uint8)
+                save_nd_array_as_image(temp_prob, prob_save_name, root_dir + '/' + names[i])
--- pymic/net_run/infer_func.py
+++ pymic/net_run/infer_func.py
@@ -7,6 +7,100 @@ import math
 import torch
 import numpy as np
 
+class Inferer(object):
+    def __init__(self, model, config):
+        self.model   = model 
+        self.config  = config
+        
+    def _infer(self, image):
+        use_sw  = self.config.get('sliding_window_enable', False)
+        if(not use_sw):
+            outputs = self.model(image)
+        else:
+            outputs = self._infer_with_sliding_window(image)
+        return outputs
+
+    def _infer_with_sliding_window(self, image):
+        window_size   = self.config['sliding_window_size']
+        window_stride = self.config['sliding_window_stride']
+        class_num     = self.config['class_num']
+        out_num       = self.config.get('output_num', 1)
+        img_full_shape = image.shape
+        img_shape = list(img_full_shape[2:])
+        img_dim   = len(img_shape)
+        if(img_dim != 2 and img_dim !=3):
+            raise ValueError("Inference using sliding window only supports 2D and 3D images")
+
+        for i in range(img_dim):
+            if window_size[i] is None:
+                window_size[i]  = img_shape[i]
+            if window_stride[i] is None:
+                window_stride[i] = window_size[i]
+
+        crop_start_list  = []
+        for w in range(0, img_shape[-1], window_stride[-1]):
+            w_min = min(w, img_shape[-1] - window_size[-1])
+            for h in range(0, img_shape[-2], window_stride[-2]):
+                h_min = min(h, img_shape[-2] - window_size[-2])
+                if(img_dim == 2):
+                    crop_start_list.append([h_min, w_min])
+                else:
+                    for d in range(0, img_shape[0], window_stride[0]):
+                        d_min = min(d, img_shape[0] - window_size[0])
+                        crop_start_list.append([d_min, h_min, w_min])
+        
+        output_shape = [img_full_shape[0], class_num] + img_shape
+        output_list  = [torch.zeros(output_shape).cuda() for i in range(out_num)]
+        pred_num_arr = torch.zeros(output_shape).cuda()
+        mask_shape = [img_full_shape[0], class_num] + window_size
+        temp_mask    = torch.ones(mask_shape).cuda()
+        
+        for c0 in crop_start_list:
+            c1 = [c0[i] + window_size[i] for i in range(img_dim)]
+            if(img_dim == 2):
+                patch_in = image[:, :, c0[0]:c1[0], c0[1]:c1[1]]
+            else:
+                patch_in = image[:, :, c0[0]:c1[0], c0[1]:c1[1], c0[2]:c1[2]]
+            patch_out = self.model(patch_in) 
+            if(not(isinstance(patch_out, (tuple, list)))):
+                patch_out = [patch_out]
+            for i in range(out_num):
+                if(img_dim == 2):
+                    output_list[i][:, :, c0[0]:c1[0], c0[1]:c1[1]] += patch_out[i]
+                    pred_num_arr[:, :, c0[0]:c1[0], c0[1]:c1[1]] += temp_mask
+                else:
+                    output_list[i][:, :, c0[0]:c1[0], c0[1]:c1[1], c0[2]:c1[2]] += patch_out[i]
+                    pred_num_arr[:, :, c0[0]:c1[0], c0[1]:c1[1], c0[2]:c1[2]] += temp_mask
+        
+        output_list = [item / pred_num_arr for item in output_list]
+        return output_list
+
+    def run(self, image):
+        tta_mode  = self.config.get('tta_mode', 0)
+        if(tta_mode == 0):
+            outputs = self._infer(image)
+        elif(tta_mode == 1): # test time augmentation with flip in 2D
+            outputs1 = self._infer(image)
+            outputs2 = self._infer(torch.flip(image, [-1]))
+            outputs3 = self._infer(torch.flip(image, [-2]))
+            outputs4 = self._infer(torch.flip(image, [-2, -1]))
+            if(isinstance(outputs1, (tuple, list))):
+                outputs = []
+                for i in range(len(outputs)):
+                    temp_out1 = outputs1[i]
+                    temp_out2 = torch.flip(outputs2[i], [-1])
+                    temp_out3 = torch.flip(outputs3[i], [-2])
+                    temp_out4 = torch.flip(outputs4[i], [-2, -1])
+                    temp_mean = (temp_out1 + temp_out2 + temp_out3 + temp_out4) / 4
+                    outputs.append(temp_mean)
+            else:
+                outputs2 = torch.flip(outputs2, [-1])
+                outputs3 = torch.flip(outputs3, [-2])
+                outputs4 = torch.flip(outputs4, [-2, -1])
+                outputs = (outputs1 + outputs2 + outputs3 + outputs4) / 4
+        return outputs
+
+
 def volume_infer(image, net, class_num, sliding_window = False,
         window_size = None, window_stride  = None, output_num = 1):
     """
