2060fe79911ee34a774ee816a83666717cec8563
Guotai Wang <wguotai@gmail.com>
2021-05-04 10:09:02 +0800

    fuse net_run_cls and net_run_seg

--- pymic/net_run/net_run.py
+++ pymic/net_run/net_run.py
@@ -2,17 +2,23 @@
 from __future__ import print_function, division
 import sys
 from pymic.util.parse_config import parse_config
+from pymic.net_run.agent_cls import ClassificationAgent
 from pymic.net_run.agent_seg import SegmentationAgent
 
 def main():
     if(len(sys.argv) < 3):
         print('Number of arguments should be 3. e.g.')
-        print('   pymic_net_run_seg train config.cfg')
+        print('   pymic_net_run train config.cfg')
         exit()
     stage    = str(sys.argv[1])
     cfg_file = str(sys.argv[2])
     config   = parse_config(cfg_file)
-    agent    = SegmentationAgent(config, stage)
+    task     = config['dataset']['task_type']
+    assert task in ['cls', 'cls_nexcl', 'seg']
+    if(task == 'cls' or task == 'cls_nexcl'):
+        agent = ClassificationAgent(config, stage)
+    else:
+        agent = SegmentationAgent(config, stage)
     agent.run()
 
 if __name__ == "__main__":
