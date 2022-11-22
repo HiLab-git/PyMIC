981d47acf0550fd93af3c15cde0ec31ba49e339b
taigw <wguotai@gmail.com>
2022-11-22 23:00:47 +0800

    update network
    unet2d_urpc is not needed as deep supervision is supported by unet2d and unet3d now

--- pymic/net/net_dict_seg.py
+++ pymic/net/net_dict_seg.py
@@ -4,7 +4,6 @@ Built-in networks for segmentation.
 
 * UNet2D :mod:`pymic.net.net2d.unet2d.UNet2D`
 * UNet2D_DualBranch :mod:`pymic.net.net2d.unet2d_dual_branch.UNet2D_DualBranch`
-* UNet2D_URPC :mod:`pymic.net.net2d.unet2d_urpc.UNet2D_URPC`
 * UNet2D_CCT  :mod:`pymic.net.net2d.unet2d_cct.UNet2D_CCT`
 * UNet2D_ScSE  :mod:`pymic.net.net2d.unet2d_scse.UNet2D_ScSE`
 * AttentionUNet2D  :mod:`pymic.net.net2d.unet2d_attention.AttentionUNet2D`
@@ -17,7 +16,6 @@ Built-in networks for segmentation.
 from __future__ import print_function, division
 from pymic.net.net2d.unet2d import UNet2D
 from pymic.net.net2d.unet2d_dual_branch import UNet2D_DualBranch
-from pymic.net.net2d.unet2d_urpc import UNet2D_URPC
 from pymic.net.net2d.unet2d_cct import UNet2D_CCT
 from pymic.net.net2d.cople_net import COPLENet
 from pymic.net.net2d.unet2d_attention import AttentionUNet2D
@@ -30,7 +28,6 @@ from pymic.net.net3d.unet3d_scse import UNet3D_ScSE
 SegNetDict = {
 	'UNet2D': UNet2D,
 	'UNet2D_DualBranch': UNet2D_DualBranch,
-	'UNet2D_URPC': UNet2D_URPC,
 	'UNet2D_CCT': UNet2D_CCT,
 	'COPLENet': COPLENet,
 	'AttentionUNet2D': AttentionUNet2D,
