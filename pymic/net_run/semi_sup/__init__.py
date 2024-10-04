b99b5de9a330f7334c0d45527b08b2b4205fefd9
taigw <wguotai@gmail.com>
2024-10-04 15:28:36 +0800

    Update __init__.py

--- pymic/net_run/semi_sup/__init__.py
+++ pymic/net_run/semi_sup/__init__.py
@@ -3,7 +3,7 @@ from pymic.net_run.semi_sup.ssl_abstract import SSLSegAgent
 from pymic.net_run.semi_sup.ssl_em import SSLEntropyMinimization
 from pymic.net_run.semi_sup.ssl_mt import SSLMeanTeacher
 from pymic.net_run.semi_sup.ssl_mcnet import SSLMCNet
-from pymic.net_run.semi_sup.ssl_cdma import SSLCDMA
+# from pymic.net_run.semi_sup.ssl_cdma import SSLCDMA
 from pymic.net_run.semi_sup.ssl_uamt import SSLUncertaintyAwareMeanTeacher
 from pymic.net_run.semi_sup.ssl_cct import SSLCCT
 from pymic.net_run.semi_sup.ssl_cps import SSLCPS
@@ -13,7 +13,7 @@ from pymic.net_run.semi_sup.ssl_urpc import SSLURPC
 SSLMethodDict = {'EntropyMinimization': SSLEntropyMinimization,
     'MeanTeacher': SSLMeanTeacher,
     'MCNet': SSLMCNet,
-    'CDMA': SSLCDMA, 
+    # 'CDMA': SSLCDMA, 
     'UAMT': SSLUncertaintyAwareMeanTeacher,
     'CCT': SSLCCT,
     'CPS': SSLCPS,
